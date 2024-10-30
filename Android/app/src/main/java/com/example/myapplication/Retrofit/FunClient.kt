a1b5a82dcec370e4fd76c58cda246b016f956e0f
Ssong0515 <406605@naver.com>
2024-10-30 18:10:27 +0900

    레파지토리 변경

--- Android/app/src/main/java/com/example/myapplication/Retrofit/FunClient.kt
+++ Android/app/src/main/java/com/example/myapplication/Retrofit/FunClient.kt
@@ -6,7 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object FunClient {
     val retrofit:FunInterface = Retrofit.Builder()
-        //TODO: ip 주소 변경 필요
         .baseUrl(Const.SERVER_BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
 //        .addConverterFactory(Json.asConverterFactory(MediaType.parse("application/json")!!))
--- Android/app/src/main/java/com/example/myapplication/utils/Const.kt
+++ Android/app/src/main/java/com/example/myapplication/utils/Const.kt
@@ -6,6 +6,5 @@ object Const {
     val SHARED_PREF_LOGIN_TRUE = "true"
     val SHARED_PREF_LOGIN_FALSE = "false"
     val SHARED_PREF_LOGIN_ID = "userId"
-
-    val SERVER_BASE_URL = "http://10.100.105.203:8080"
+    val SERVER_BASE_URL = "http://15.164.183.44:8081"
 }
