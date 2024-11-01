c668e84497590a18db374dbecb2226af0712d3cc
Ssong0515 <406605@naver.com>
2024-11-01 23:27:15 +0900

    이미지 변경 및 DB  숨김 처리

--- Android/app/src/main/java/com/example/myapplication/utils/Const.kt
+++ Android/app/src/main/java/com/example/myapplication/utils/Const.kt
@@ -6,6 +6,6 @@ object Const {
     val SHARED_PREF_LOGIN_TRUE = "true"
     val SHARED_PREF_LOGIN_FALSE = "false"
     val SHARED_PREF_LOGIN_ID = "userId"
-//    val SERVER_BASE_URL = "http://15.164.183.44:8081"
-    val SERVER_BASE_URL = "http://10.100.105.203:8081"
+    val SERVER_BASE_URL = "http://15.164.183.44:8081"
+//    val SERVER_BASE_URL = "http://192.168.219.104:8081"
 }
